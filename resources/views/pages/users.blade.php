ead9a290870f98350f706eec4597f3de8917725d
FeryPermana <muhammad.permana@students.amikom.ac.id>
2020-11-22 12:41:09 +0700

    first commit

---
+++ resources/views/pages/users.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts/global')
+@section('page')
+<h1 class="judul">Ini halaman User</h1>
+@endsection
+
+@push('after-css')
+<style>
+    .judul {
+        color: blue;
+    }
+</style>
+@endpush
